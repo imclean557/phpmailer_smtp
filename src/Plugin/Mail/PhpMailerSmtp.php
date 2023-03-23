a803576d1f0620df4588b3dd718e4ef57c3f369b
skylord <38707-skylord@users.noreply.drupalcode.org>
2023-03-24 08:06:32 +1100

    Issue #3349815 by skylord: Wrong charset after update

--- src/Plugin/Mail/PhpMailerSmtp.php
+++ src/Plugin/Mail/PhpMailerSmtp.php
@@ -174,6 +174,7 @@ class PhpMailerSmtp extends PHPMailer implements MailInterface, ContainerFactory
    */
   public function smtpInit() {
     $this->IsSMTP();
+    $this->Reset();
     $this->SMTPAutoTLS = FALSE;
 
     $this->Host = $this->config->get('smtp_host');
